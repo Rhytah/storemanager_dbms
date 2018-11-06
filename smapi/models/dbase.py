a65256bc6230619e45242673aeb545d4dc031d0e
Rhytah <ritanamono@gmail.com>
2018-11-06 11:17:54 +0300

    have single connection

--- smapi/models/dbase.py
+++ smapi/models/dbase.py
@@ -17,44 +17,36 @@ class Databasehandler:
         self.conn = psycopg2.connect(connection_credentials)
         self.conn.autocommit = True
         self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
-        try:
-
-            self.conn = psycopg2.connect(connection_credentials)
-            self.conn.autocommit = True
-            self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
-            print("\n\n Database Connected\n\n")
+        print("\n\n Database Connected\n\n")
      
-            usercmd="""CREATE TABLE IF NOT EXISTS users(
-                user_id SERIAL PRIMARY KEY,
-                username VARCHAR (30),
-                password VARCHAR (10),
-                role BOOLEAN DEFAULT FALSE NOT NULL)"""
-            self.cursor.execute(usercmd)
-        
-            pdtcmd="""CREATE TABLE IF NOT EXISTS products(
-                product_id SERIAL PRIMARY KEY,
-                product_name VARCHAR(20),
-                unit_price INT,
-                category VARCHAR(15),
-                stock INT)"""
-            self.cursor.execute(pdtcmd)
-            salecmd="""CREATE TABLE IF NOT EXISTS sales(
-                sale_id SERIAL PRIMARY KEY ,
-                entered_by VARCHAR,
-                product_id INT REFERENCES products (product_id),
-                cost INT,
-                quantity INT,
-                total INT)"""
-            self.cursor.execute(salecmd)
-            adminuser=f"""
-                    INSERT INTO users(username, password, role)
-                    VALUES('admin','admin' ,TRUE)
-                    """
-            self.cursor.execute(adminuser)
+        usercmd="""CREATE TABLE IF NOT EXISTS users(
+            user_id SERIAL PRIMARY KEY,
+            username VARCHAR (30),
+            password VARCHAR (10),
+            role BOOLEAN DEFAULT FALSE NOT NULL)"""
+        self.cursor.execute(usercmd)
+    
+        pdtcmd="""CREATE TABLE IF NOT EXISTS products(
+            product_id SERIAL PRIMARY KEY,
+            product_name VARCHAR(20),
+            unit_price INT,
+            category VARCHAR(15),
+            stock INT)"""
+        self.cursor.execute(pdtcmd)
+        salecmd="""CREATE TABLE IF NOT EXISTS sales(
+            sale_id SERIAL PRIMARY KEY ,
+            entered_by VARCHAR,
+            product_id INT REFERENCES products (product_id),
+            cost INT,
+            quantity INT,
+            total INT)"""
+        self.cursor.execute(salecmd)
+        adminuser=f"""
+                INSERT INTO users(username, password, role)
+                VALUES('admin','admin' ,TRUE)
+                """
+        self.cursor.execute(adminuser)
 
-        except Exception as e:
-            print(e)
-            print("Connection failed")
 
     def search_user(self,username):
         cmd="SELECT * FROM users WHERE username='{}'".format(username)
